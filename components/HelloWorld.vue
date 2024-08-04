9e40f2c4f90c80526cb1108d49c236727e8e7dd4
xihe <im0k@outlook.com>
2024-08-04 17:29:58 +0800

    init

---
+++ components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { version } from '../package.json';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p> 网页小组件开发中... </p>
+  <p class="read-the-docs">{{ version }}</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
